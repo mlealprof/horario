759f56fc634bd6bbdd3cd38749253589d9fe0e57
lucasolid <lukasdragneel@gmail.com>
2020-02-18 19:52:07 -0300

    LuanAT

--- Enviar Direto.bat
+++ Enviar Direto.bat
@@ -2,7 +2,7 @@ cd /c/xampp/htdocs/projeto/horario
 git fetch upstream
 git add .
 pause
-git commit -m "WyssAtualizando"
+git commit -m "LuanAT"
 git push
 pause
 echo ###Arquivo enviado e atualizado###
