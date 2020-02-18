a417b47791f5a7c6f1ce3d4a9471606f5337a412
yago81 <yagog3.g3@gmail.com>
2020-02-18 18:36:37 -0300

    WyssAtualizando

---
+++ Wyss Bat para enviar direto.bat
@@ -0,0 +1,10 @@
+cd /c/xampp/htdocs/projeto/horario
+git fetch upstream
+git add .
+pause
+git commit -m "WyssAtualizando"
+pause
+git push
+-------Arquivo enviado e atualizado------
+------------------WYSS-------------------
+pause
