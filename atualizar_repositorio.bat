2e8d7ae5c898cc8e98d45567053bd36e7e0bfc3f
mlealprof <marcioleal.prof@gmail.com>
2020-02-19 19:44:56 -0300

    Atualizando arquivos Bats do Wemerson

--- atualizar_repositorio.bat
+++ atualizar_repositorio.bat
@@ -1,10 +1,13 @@
 cd /c/xampp/htdocs/projeto/horario
+pause
 git fetch upstream
+pause
+git merge upstream/master
+pause
 git add .
 pause
-git commit -m "WyssAtualizando"
-git push
+git commit -m "Artualização direta do repositório"
 pause
+git push
 echo ###Arquivo enviado e atualizado###
-echo ###WYSS###
 pause
