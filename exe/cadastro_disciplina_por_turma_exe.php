81d8faba96e38f6c43425ba3cd22a2ac8326949f
antoniocsk8 <antoniorox2011@gmail.com>
2020-02-20 19:33:14 -0300

    Mensagem de cadastro Disciplina por turma

--- exe/cadastro_disciplina_por_turma_exe.php
+++ exe/cadastro_disciplina_por_turma_exe.php
@@ -12,4 +12,16 @@
 	$inserir_disciplina_turma = "INSERT INTO disciplia_por_turma (codigo_disciplina, codigo_turma, codigo_professor, carga_horaria) VALUES ('$codigo_disciplina', '$codigo_turma', '$codigo_professor', '$carga_horaria')";
 	mysqli_query($conexao, $inserir_disciplina_turma);
 
+	if (mysqli_query($conexao, $inserir_disciplina_turma)) {
+			echo '<script type="text/javascript">
+			  alert("Disciplina por turma cadastrada com sucesso !");
+			  window.location.href="../index.php"
+			  </script>';
+		}else{
+			echo '<script type="text/javascript">
+			  alert("Erro ao cadastrar disciplina por turma !");
+			  window.location.href="../cadastro_disciplina_por_turma_exe.php"
+			  </script>';
+		}
+
   ?>
