980f6ecf93dc445a4e7bc4f0da260d1ce182fc07
Rudsonhcs19 <rudson2015tri@gmail.com>
2020-02-20 19:48:25 -0300

    Rodapé atualizado

--- footer.php
+++ footer.php
@@ -7,12 +7,6 @@
         &copy; Copyright <strong>TimeTable</strong>. Todos os Direitos Reservados
       </div>
       <div class="credits">
-        <!--
-          All the links in the footer should remain intact.
-          You can delete the links only if you purchased the pro version.
-          Licensing information: https://bootstrapmade.com/license/
-          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Reveal
-        -->
         Designed by T.I 2018</a>
       </div>
     </div>
