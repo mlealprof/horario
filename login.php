5d680c447edb82706584a27b0ae5743a578182a1
migueltiee <mirandatecltda@gmail.com>
2020-02-20 23:05:00 -0300

    Teste

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+	require("header.php");
+?>
+	
+<body>
+	<section id="intro">
+		<div class="babado-container intro-content">
+			Usuário
+			<input type="text" name="" size="20" maxlength="50"><br>
+			Senha
+			<input type="text" name="" size="20" maxlength="50">
+			<a href="">Esqueceu sua senha?</a><br>
+			<div>
+	        	<a href="#about" class="btn-get-started scrollto" >Entrar</a>
+	        	<a href="#portfolio" class="btn-projects scrollto" >Cadastrar</a>
+	      </div>
+	    </div>  
+	</section>    
+</body>
+<?php
+	require("footer.php");
+?>
