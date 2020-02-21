e306356258bcc0b4e1d983f780b7a17aae030801
Alunos TI <fabianoarcos@hotmail.com>
2020-02-20 22:08:43 -0300

    Artualiza├º├úo direta do reposit├│rio

---
+++ exe/cadastrar_usuario.exe.php
@@ -0,0 +1,24 @@
+<?php
+	// Conexão com o banco de dados
+	require 'conexao_exe.php';
+
+	// Pegando dados via POST
+	$usuario = $_POST['usuario'];
+	$senha = $_POST['senha'];
+	
+
+	// Inserindo dados na tabela
+	$inserir_usuario = "INSERT INTO usuario (usuario,senha,) VALUES ('$usuario', '$senha')";
+
+	if (mysqli_query($conexao, $inserir_usuario)) {
+		echo '<script type="text/javascript">
+			alert("");
+			window.location.href="../cpanel.php";
+			</script>';
+	} else {
+		echo '<script type="text/javascript">
+			alert("Falha no login. Verifique os dados inseridos.");
+			window.location.href="../usuario.php";
+			</script>';
+	}
+?>	
