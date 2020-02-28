2a98fe9bd0b7804ce3e95db45ec5a130cee820a6
yago81 <yagog3.g3@gmail.com>
2020-02-28 20:47:29 -0300

    ATT de correcoes de erros

--- atualizar_repositorio.bat
+++ atualizar_repositorio.bat
@@ -1,10 +1,20 @@
 ﻿cd /c/xampp/htdocs/projeto/horario
+
+pause
+
 git fetch upstream
+
 pause
 git add .
+
 pause
+
 git commit -m "comando direto"
+
 pause
+
 git push
+
 echo ###Arquivo enviado e atualizado###
+
 pause
