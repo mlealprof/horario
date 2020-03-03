ff53e8dc09656f08ae0ed7f2c0dd822be921eac1
antoniocsk8 <antoniorox2011@gmail.com>
2020-03-03 19:47:46 -0300

    Artualiza├º├úo direta do reposit├│rio

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
-
   <!-- Favicons -->
   <link href="img/favicon.png" rel="icon">
   <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
