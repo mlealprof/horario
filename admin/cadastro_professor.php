73ec3ec977ad4b40bcc7334a6cd1e90c042a0db9
LauraSantos19 <lauracristinamsantos@gmail.com>
2020-03-03 20:34:30 -0300

    alteracao do cadastro de professores

--- admin/cadastro_professor.php
+++ admin/cadastro_professor.php
@@ -1,7 +1,11 @@
-
+<<<<<<< HEAD
 <!--==========================
   Cadastro de Professor
   ============================-->
+
+
+  
+
 <h1 align="center">Cadastro Professores</h1>
 
     <section class="img_cadastros">
@@ -18,8 +22,6 @@
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Digite o nome da Escola">
           </div>
 
-<<<<<<< HEAD
-=======
         <form>
 =======
 <main>
@@ -89,10 +91,50 @@
               </div>
             </div>
           </div>
->>>>>>> upstream/master
         </form>
-      </div>
+
+      </div><br>
+      <!-- Fim cadastro -->
+
+      <!-- Tabela de cadastrados -->
+      <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Ordem</th>
+            <th scope="col">Posição</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+            $cod_escola = $_SESSION['cod_escola'];
+            $consulta = "SELECT * FROM horarios WHERE cod_escola = '$cod_escola'";
+            $resultado = mysqli_query($conexao, $consulta);
+            if (mysqli_num_rows($resultado) == 0) {
+          ?>
+            <tr>
+              <td colspan="5" class="text-center"><?php echo "Nenhuma disciplina cadastrada."; ?></td>
+            </tr>
+          <?php
+            } else {
+              while ($array = mysqli_fetch_assoc($resultado)) {
+          ?>
+            <tr>
+              <td><?php echo $array['cod_horarios']; ?></td>
+              <td><?php echo $array['ordem']; ?></td>
+              <td><?php echo $array['posicao']; ?></td>
+              <td><?php echo $array['descricao']; ?></td>
+              <td><button type="button" class="btn btn-danger">Excluir</button></td>
+            </tr>
+          <?php
+              }
+            }
+          ?>
+        </tbody>
+      </table>
+      <!-- Fim tabela de cadastrados -->
     </div>
   </section>
-<main>
-  
+</main>
