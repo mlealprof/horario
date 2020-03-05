21fc5be9af86f8360c18a5fbdf265440a617f357
shimotochan <rafael_2010zz@hotmail.com>
2020-03-05 20:02:32 -0300

    numero 32 combobox no tipo de ensino etc..

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -7,7 +7,7 @@
 	<section id="intro">
 
 	   <form action="exe/login_exe.php" method="Post">
-		<div class="babado-container intro-content">
+		<div class="babado-container intro-content font">
 		<h2>Digite seu Email</h2>
 		Informe o Email cadastrado e enviaremos sua senha temporaria.
 			<input type="text" name="email" size="50" maxlength="50" placeholder="Digite seu email"><br>
