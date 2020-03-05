ed4e8480fad4563212238952244448c401f554a4
Peregrinolunar <peregrinolunar@gmail.com>
2020-03-05 20:20:19 -0300

    At. Online para PC

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -9,7 +9,7 @@
 	   <form action="exe/login_exe.php" method="Post">
 		<div class="babado-container intro-content font">
 		<h2>Recupeção de senha:</h2>
-		<p>Informe o Email cadastrado e enviaremos sua senha temporaria.</p>
+		<p>Informe o Email cadastrado e enviaremos uma senha temporaria.</p>
 			<input type="text" name="email" size="50" maxlength="50" placeholder="Digite seu email"><br>
 			<div>
 	        	 <button type="submit" class="btn btn-primary tamanho_button">Enviar</button>
